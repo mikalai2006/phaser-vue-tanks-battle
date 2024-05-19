f589544e91b87b7043b2908697c0d4abce2c7c39
Mikalai Parakhnevich <composer.opencart@gmail.com>
2024-05-19 08:40:47 +0300

    feat: refactoring

--- rezerv/Team.ts
+++
@@ -1,6 +0,0 @@
-import { Types, defineComponent } from 'bitecs'
-
-export const Team = defineComponent({
-  maxHealth: Types.ui8,
-  health: Types.ui8
-})
--- rezerv/_Muzzle.ts
+++
@@ -1,7 +0,0 @@
-// import { Types, defineComponent } from 'bitecs'
-
-// export const Muzzle = defineComponent({
-//   level: Types.ui32,
-//   distanceShot: Types.ui32,
-//   speedShot: Types.ui32
-// })
